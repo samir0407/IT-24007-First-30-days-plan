b140ac50fdb7f4fe38632fdbcf26990076a9afe7
Samir Tahrian <samirtahrian1002@gmail.com>
2026-04-20 08:12:56 +0600

    Add files via upload

---
+++ Day-1-plan/example2/Main.java
@@ -0,0 +1,16 @@
+
+package example2;
+
+public class Main {
+
+    public static void main(String[] args){
+        Student s1 = new Student();
+        s1.Name = "MD.Samir Tahrian";
+        s1.Roll = "IT24007";
+        s1.District = "Lalmonirhat";
+        s1.Number = "01568456332";
+        s1.Show();
+    }
+}
+
+
---
+++ Day-1-plan/example2/Student.java
@@ -0,0 +1,20 @@
+
+package example2;
+
+public class Student {
+   
+    
+    String Name;
+    String Roll;   // এখানে String রাখা হয়েছে যাতে "IT24007" এর মতো মান দেওয়া যায়
+    String District;
+    String Number;
+    
+    void Show() {
+        System.out.println(Name);
+        System.out.println(Roll);
+        System.out.println(District);
+        System.out.println(Number);
+    }
+}
+
+
