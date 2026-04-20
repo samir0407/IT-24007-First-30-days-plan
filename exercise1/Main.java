555514e97c9d9f98d7bb2e77f597c4ff8ec11c6a
Samir Tahrian <samirtahrian1002@gmail.com>
2026-04-20 08:06:41 +0600

    Day-1-plan/Main.java

---
+++ exercise1/Main.java
@@ -0,0 +1,13 @@
+package exercise1;
+
+    public class Main{
+    public static void main(String[] args){
+  Car myCar=new Car();
+  myCar.color="White";
+  myCar.model="Nissan";
+  myCar.year=2023;
+  myCar.displayInfo();
+    }
+}
+    
+
