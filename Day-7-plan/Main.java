7f4e7e43b9007336150037a9b2ec55fdef827f2e
Samir Tahrian <samirtahrian1002@gmail.com>
2026-04-28 19:18:35 +0600

    Add files via upload

---
+++ Day-7-plan/Main.java
@@ -0,0 +1,17 @@
+class Outer {
+    int x = 10;
+
+    class Inner {
+        void show() {
+            System.out.println(x);
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Outer o = new Outer();
+        Outer.Inner i = o.new Inner();
+        i.show();
+    }
+}
