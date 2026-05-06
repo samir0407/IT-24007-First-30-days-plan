03b3ed5ac2b85ede157452917f1da9593b938cb5
Samir Tahrian <samirtahrian1002@gmail.com>
2026-05-06 10:58:26 +0600

    Update leetcode.java

--- LT # 1 Solving Problem Harshad Number CHallenge/Task # 1/java code/Code/leetcode.java
+++ LT # 1 Solving Problem Harshad Number CHallenge/Task # 1/java code/Code/leetcode.java
@@ -1,13 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package assignment_01;
+class Solution {
+    public int sumOfTheDigitsOfHarshadNumber(int num) {
 
-/**
- *
- * @author samir
- */
-public class leetcode {
-    
+        int temp=num;
+        int rem,sum=0;
+        while (temp>0)
+        {
+            rem=temp%10;
+            temp=temp/10;
+            sum=sum+rem;
+            
+        }
+        if (num%sum==0){
+            return sum;
+        }
+        else {
+            return -1;
+        }
+    }
 }
+
+    
